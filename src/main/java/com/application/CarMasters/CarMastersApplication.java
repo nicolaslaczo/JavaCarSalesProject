a49a1dbe899580d31201cea609a6a4873030863d
Nicolas Laczo <nicolas.laczo@gmail.com>
2024-03-31 17:57:44 +0200

    Created new project

---
+++ src/main/java/com/application/CarMasters/CarMastersApplication.java
@@ -0,0 +1,13 @@
+package com.application.CarMasters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarMastersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarMastersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/CarMasters/CarMastersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.CarMasters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarMastersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
